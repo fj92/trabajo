a7f8d09a5a3ae5ea900d1ed1105274148d6dfb22
francisco argumedo <fargumedo761@gmail.com>
2020-08-26 23:41:32 -0500

    Mi primer commit

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+def par_impar():
+    valor = int(input("ingrese un numero : "))
+    if valor%2==0:
+        print("es par")
+    else:
+        print("es impar")
+    return()
+
+def main():
+    par_impar()
+if __name__ == "__main__":
+    main()
+
+   
+
